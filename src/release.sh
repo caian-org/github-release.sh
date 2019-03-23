4b4f20245ebd93b80e035325a3d1d7ed4f28c41e
Caian R. Ertl <caian.ertl@rivendel.com.br>
2019-03-23 04:37:02 -0300

    feat: include some logging messages

--- src/release.sh
+++ src/release.sh
@@ -2,14 +2,19 @@
 
 set -e
 
+# Output an error message and exit with status code 1
 die() {
-    printf "n\e[31mERROR:\e[m %s\n" "$@" >&2
+    printf 'n\e[31mERROR:\e[m %s\n' "$@" >&2
     exit 1
 }
 
+# Log something with a fancy green arrow
 log() {
-    printf "\e[32m=>\e[m %s\n" "$@"
+    printf '\e[32m=>\e[m %s' "$@"
 }
+
+
+printf '\e[36m%s\e[m\n\n' "release.sh has started"
 
 # Get the remote URL of the git repository
 GIT_REMOTE_URL="$(git remote get-url --all origin | sed 's/\.git//g')"
@@ -18,6 +23,7 @@ GIT_REMOTE_URL="$(git remote get-url --all origin | sed 's/\.git//g')"
 GIT_PROJ_INFO=( $(echo "$GIT_REMOTE_URL" | awk -F '/' '{print $4; print $5}') )
 GIT_USER="${GIT_PROJ_INFO[0]}"
 GIT_REPO="${GIT_PROJ_INFO[1]}"
+log "using project \"$GIT_REPO\" of user \"$GIT_USER\"\n"
 
 # <github.com/@user/@repo/commit/>
 GIT_COMMIT_URL="$(printf "%s/commit/" "$GIT_REMOTE_URL")"
@@ -47,6 +53,7 @@ LAST_REF="$(echo "$GIT_TAGS" | tail -n 1)"
 
 # Compare the last tag against the last reference (the master branch or the
 # penultimate tag). Output the content in HTML to a temporary file
+log "generating changelog from \"$PENULT_REF\" to \"$LAST_REF\"... "
 {
     echo "<h1>Changelog</h1><ul>";
     git log --pretty=oneline "${PENULT_REF}..${LAST_REF}" | \
@@ -58,7 +65,10 @@ LAST_REF="$(echo "$GIT_TAGS" | tail -n 1)"
 # Get the changelog content
 CHANGELOG="$(cat /tmp/rcl)"
 rm /tmp/rcl
+echo "done!"
 
+# Make a POST request to GitHub's release API
+log "creating release... "
 RESPONSE="$(
     curl --user "${GIT_USER}":"${GIT_TOKEN}" \
          --request POST \
@@ -77,5 +87,6 @@ RESPONSE="$(
          }
 END
 )"
+echo "done!"
 
 echo "$RESPONSE"
