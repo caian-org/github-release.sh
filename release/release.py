0d9e9284b7f81b63ba58fb493c873d489f77e210
Caian R. Ertl <hi@caian.org>
2021-03-05 17:38:43 -0300

    fix: a couple of linting issues

--- release/release.py
+++ release/release.py
@@ -42,38 +42,42 @@ def info(msg):
     print('\033[32m=>\033[0m {}'.format(msg))
 
 
-def exec(c):
+def do_cmd(cmdl):
+    if not isinstance(cmdl, list):
+        cmdl = cmdl.split(' ')
+
     try:
-        proc = Popen(c, stdout=PIPE, stderr=PIPE)
-    except Exception:
-        return True, None
+        proc = Popen(cmdl, stdout=PIPE, stderr=PIPE)
+    except Exception as err:
+        return err, None
 
     out, err = proc.communicate()
     if err:
-        return True, None
+        return err, None
 
     return None, out.decode('utf-8').rstrip()
 
 
 def git_remote():
-    return exec(['git', 'remote', 'get-url', '--all', 'origin'])
+    return do_cmd('git remote get-url --all origin')
 
 
 def git_tag():
-    return exec(['git', 'tag', '--sort=committerdate'])
+    return do_cmd('git tag --sort=committerdate')
 
 
 def git_log(tags):
-    return exec(['git', 'log', '--pretty=oneline', '{0}..{1}'.format(tags['penult'], tags['last'])])
+    return do_cmd('git log --pretty=online {}..{}'.format(tags['penult'], tags['last']))
 
 
 def identify_provider(prefix):
-    if 'github.com' == prefix:
+    if prefix == 'github.com':
         return 'github'
-    elif 'gitlab.com' == prefix:
+
+    if prefix == 'gitlab.com':
         return 'gitlab'
-    else:
-        return None
+
+    return None
 
 
 def generate_changelog(data):
@@ -91,14 +95,14 @@ def generate_changelog(data):
 
 
 def get_remote_data(remote):
-    # --------------------------------------------------
     protocol = 'https'
 
     if 'https://' not in remote:
         protocol = 'ssh'
 
-    # --------------------------------------------------
-    commit_url, provider, aux = '', '', ''
+    aux = ''
+    commit_url = ''
+    provider = ''
 
     if protocol == 'https':
         aux = remote.split('/')
@@ -111,14 +115,13 @@ def get_remote_data(remote):
     if not provider:
         return True, 'unsupported provider'
 
-    # --------------------------------------------------
     auz = remote.split('{}.com'.format(provider))
     auz = auz[1][1:]
     auz = auz[: len(auz) - 4]
     commit_url = 'https://{0}.com/{1}/commit'.format(provider, auz)
 
-    # --------------------------------------------------
-    user, repo = '', ''
+    user = ''
+    repo = ''
 
     if protocol == 'https':
         user = aux[3]
@@ -140,18 +143,14 @@ def get_remote_data(remote):
 
 
 def post_request_with_auth(data):
-    # --------------------------------------------------
     payload = json.dumps(data['payload']).encode('utf-8')
 
-    # --------------------------------------------------
     key, val = data['auth']
 
-    # --------------------------------------------------
     req = Request(data['url'], data=payload)
     req.add_header('Content-Type', 'application/json')
     req.add_header(key, val)
 
-    # --------------------------------------------------
     res = None
     try:
         res = urlopen(req).read().decode()
@@ -163,16 +162,12 @@ def post_request_with_auth(data):
 
 
 def create_github_release(data):
-    toplevel_domain = 'https://api.github.com'
-    if TEST_MODE:
-        toplevel_domain = MOCK_SERVER
+    toplevel_domain = TEST_MODE or 'https://api.github.com'
 
-    # --------------------------------------------------
     url = '{0}/repos/{1}/{2}/releases'.format(
         toplevel_domain, data['git']['user'], data['git']['repo']
     )
 
-    # --------------------------------------------------
     payload = {
         'target_commitish': 'master',
         'tag_name': data['git']['tags']['last'],
@@ -182,12 +177,10 @@ def create_github_release(data):
         'prerelease': False,
     }
 
-    # --------------------------------------------------
     auth = '{}:{}'.format(data['git']['user'], data['token'])
     auth = base64.b64encode(auth.encode('utf-8'))
     auth = 'Basic {}'.format(auth.decode('ascii'))
 
-    # --------------------------------------------------
     return post_request_with_auth({'url': url, 'payload': payload, 'auth': ('Authorization', auth)})
 
 
@@ -196,48 +189,38 @@ def create_gitlab_release(data):
     if TEST_MODE:
         toplevel_domain = 'http://localhost:8080'
 
-    # --------------------------------------------------
-    proj_path = data['git']['commit_url'].split('https://{}.com/'.format(data['git']['provider']))[
-        1
-    ]
+    proj_path = data['git']['commit_url'].split('https://{}.com/'.format(data['git']['provider']))[1]
 
     proj_path = proj_path[: len(proj_path) - 7]
     proj_path = quote_plus(proj_path)
 
     url = '{}/api/v4/projects/{}/releases'.format(toplevel_domain, proj_path)
 
-    # --------------------------------------------------
     payload = ''
 
-    # --------------------------------------------------
     return post_request_with_auth(
         {'url': url, 'pauload': payload, 'auth': ('PRIVATE-TOKEN', data['token'])}
     )
 
 
 def create_release(data):
-    # --------------------------------------------------
     if data['git']['provider'] == 'github':
         return create_github_release(data)
 
-    else:
-        return create_gitlab_release(data)
+    return create_gitlab_release(data)
 
 
 def main():
     print('\n\033[36m{}\033[0m\n'.format('release.py has started'))
 
-    # --------------------------------------------------
     token = environ.get('RELEASE_AUTH_TOKEN')
     if not token:
         die('token is undefined')
 
-    # --------------------------------------------------
     err, remote = git_remote()
     if err:
         die('unable to get the remote URL')
 
-    # --------------------------------------------------
     err, data = get_remote_data(remote)
     if err:
         die(data)
@@ -249,14 +232,12 @@ def main():
         )
     )
 
-    # --------------------------------------------------
     err, tags = git_tag()
     if err:
         die('no tags found')
 
     tags = tags.split('\n')
 
-    # --------------------------------------------------
     data['tags'] = {}
     data['tags']['last'] = tags[-1]
 
@@ -265,7 +246,6 @@ def main():
     else:
         data['tags']['penult'] = tags[-2]
 
-    # --------------------------------------------------
     info(
         'generating changelog from "{0}" to "{1}"...'.format(
             data['tags']['penult'], data['tags']['last']
@@ -284,7 +264,6 @@ def main():
         }
     )
 
-    # --------------------------------------------------
     info('creating release...')
     err, res = create_release(
         {
